2a65c1d7499e8cd4b27f0533b39b0d1e115acad4
Mouad Rarhib <mouad159mouad@gmail.com>
2026-08-04 15:25:46 +0100

    feat(project-service): log a message on successful startup
    Makes it obvious from the console when the service is actually up,
    instead of scanning Spring Boot's default startup banner.

--- project-service/src/main/java/com/tenanthub/project/ProjectServiceApplication.java
+++ project-service/src/main/java/com/tenanthub/project/ProjectServiceApplication.java
@@ -1,13 +1,18 @@
 package com.tenanthub.project;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectServiceApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(ProjectServiceApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectServiceApplication.class, args);
+		log.info("Project Service started successfully");
 	}
 
 }
