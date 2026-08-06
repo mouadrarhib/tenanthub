d6779c140b2a43012b524d44b8a49519273c1bd0
Mouad Rarhib <mouad159mouad@gmail.com>
2026-08-06 20:35:23 +0100

    feat(tenant-service): log a message on successful startup

--- tenant-service/src/main/java/com/tenanthub/tenant/TenantServiceApplication.java
+++ tenant-service/src/main/java/com/tenanthub/tenant/TenantServiceApplication.java
@@ -1,13 +1,18 @@
 package com.tenanthub.tenant;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TenantServiceApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(TenantServiceApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(TenantServiceApplication.class, args);
+		log.info("Tenant Service started successfully");
 	}
 
 }
